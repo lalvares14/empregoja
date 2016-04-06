a3ce2527072a70c3ec3dc1ecf6af0c9bdaa0660e
Lucas Machado <lucas.dpmachado@gmail.com>
2016-04-05 22:27:55 -0300

    add feature incomplet

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,13 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    job = Job.find params[:id]
+    redirect_to job
+  end
 end
