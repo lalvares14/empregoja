eece8347b0b549ce395eb3ed70aaceb8a990eaf7
Lucas Machado <lucas.dpmachado@gmail.com>
2016-04-11 21:25:37 -0300

    user_create_valid_job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,27 @@
 class JobsController < ApplicationController
+  before_action :get_companies, only: [:new, :create]
+
   def show
     @job = Job.find(params[:id])
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
   end
 
   def create
-    @job = Job.create(job_params)
-    redirect_to @job
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render :new
+    end
   end
+
+  def get_companies
+    @companies = Company.all
+  end
+
 
   private
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
+
+  validates :title, :category, :description, :location, presence: true
   belongs_to :company
 end
