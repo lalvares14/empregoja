e44c9048e60fe1bd2d1020fb830841673bf576a6
Lucas Machado <lucas.dpmachado@gmail.com>
2016-04-14 21:41:38 -0300

    add feature: recent_jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,9 @@ class Job < ActiveRecord::Base
   belongs_to :category
 
   validates :title, :category, :description, :location, presence: true
+
+  def recent?
+    created_at > 6.days.ago
+  end
+
 end
