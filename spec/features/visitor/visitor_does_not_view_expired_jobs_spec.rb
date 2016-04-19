2914b0a0376fbf73dc8bca0b1678d3649d5a2da5
Lucas Machado <lucas.dpmachado@gmail.com>
2016-04-18 21:13:36 -0300

    add: feature vaga expirada

--- spec/features/visitor/visitor_does_not_view_expired_jobs_spec.rb
+++ spec/features/visitor/visitor_does_not_view_expired_jobs_spec.rb
@@ -31,4 +31,29 @@ feature 'Visitor does not view expired jobs' do
     expect(page).to have_content('São Paulo')
     expect(page).not_to have_content('Vaga de QA')
   end
+
+  scenario 'visitor visit expired job page and views Vaga Expirada!' do
+    company = Company.create(name: 'Campus Code',
+                            location: 'São Paulo',
+                            mail: 'contato@campus.com.br',
+                            phone: '2369-3476')
+
+    category = Category.create(name: 'Desenvolvedor')
+
+    job = nil
+
+    travel_to 90.days.ago do
+      job = Job.create(title: 'Vaga de QA',
+                 category: category,
+                 company: company,
+                 description: 'QA Junior com ao menos um projeto',
+                 location: 'São Paulo')
+
+    end
+
+    visit job_path(job)
+
+    expect(page).to have_content('Vaga Expirada!')
+
+  end
 end
